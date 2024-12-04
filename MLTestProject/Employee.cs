a3f5582bc3feec7ba96f1c48ebdddaff7a0f1328
Abhishek kushwaha <kushabhee@icloud.com>
2024-12-04 21:22:38 +0545

    Update Employee.cs

--- MLTestProject/Employee.cs
+++ MLTestProject/Employee.cs
@@ -11,5 +11,6 @@ namespace MLTestProject
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public int Age{get;set;}
     }
 }
